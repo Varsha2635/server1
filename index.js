32d5d7464fdef05809a55101da2ee7813583282a
Varsha Kundarapu <123001134+Varsha2635@users.noreply.github.com>
2025-07-20 21:40:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ mongoose.connection.once("open", () => {
 // Middleware
 const cors = require("cors");
 app.use(cors({
-  origin: "*",
+  origin: "https://resilient-bienenstitch-4f77d0.netlify.app",
   credentials: true
 }));
 app.use(express.json());
